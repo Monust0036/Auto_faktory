7265b5804c3a82503d087a3b4beee8bae69d46fe
Monust0036 <monusveltosest0036@gmail.com>
2020-11-28 01:23:50 +0530

    update url

--- src/App.js
+++ src/App.js
@@ -10,20 +10,6 @@ import Favorite from './Compo/Favorite'
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 import NotFoundPage from './Pages/404';
 
-// import Menu from './Compo/Menubar'
-// import Teams from './Compo/HomePage/Teams'
-// import Theorychange from './Compo/HomePage/Changes';
-// import Header from './Compo/HomePage/Header'
-// import Presssupported from './Compo/HomePage/PressSupported';
-// import Footer from './Compo/Footer';
-// import ReacentCar from './Compo/HomePage/recentCar'
-// import Carousel from './Compo/HomePage/caraousel/Carousel.js'
-// // import FirstStep from './Compo/FisrtStep';
-// import Lovestory from "./Compo/HomePage/loveStory"
-// import Carshowroom from './Compo/HomePage/carshowroom';
-// import history from  './history';
-// console.log(history, "rRRRRRRRRRRRRRRRRRRRRRRR")
-
 class App extends Component {
   constructor(props) {
     super(props);
